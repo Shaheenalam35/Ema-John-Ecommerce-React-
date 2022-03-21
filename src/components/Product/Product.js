e4b7ed491b9d556ba9efa1e1bca1ce62349cc3d2
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-21 22:44:03 +0100

    Cart added with css style

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,6 +5,19 @@ const Product = (props) => {
   return (
     <div className="product">
       <img src={img} alt="" />
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price: {price}</p>
+        <p>
+          <small>Seller: {seller} </small>
+        </p>
+        <p>
+          <small>Ratings: {ratings} stars </small>
+        </p>
+      </div>
+      <button className="btn-cart">
+        <p>Add To Cart</p>
+      </button>
     </div>
   );
 };
