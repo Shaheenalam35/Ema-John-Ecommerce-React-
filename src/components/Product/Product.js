492ca58138546070a6f3eba6982b0459daaf8945
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-22 16:38:47 +0100

    Click handler btn added and item pass to cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Product.css";
 const Product = (props) => {
-  const { name, img, seller, price, ratings } = props.product;
+  const { handleAddToCart, product } = props;
+  const { name, img, seller, price, ratings } = product;
+
   return (
     <div className="product">
       <img src={img} alt="" />
       <div className="product-info">
         <p className="product-name">{name}</p>
-        <p>Price: {price}</p>
+        <p>Price: ${price}</p>
         <p>
           <small>Seller: {seller} </small>
         </p>
@@ -15,7 +17,7 @@ const Product = (props) => {
           <small>Ratings: {ratings} stars </small>
         </p>
       </div>
-      <button className="btn-cart">
+      <button onClick={() => handleAddToCart(product)} className="btn-cart">
         <p>Add To Cart</p>
       </button>
     </div>
