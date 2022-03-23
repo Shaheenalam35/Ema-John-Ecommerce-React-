1a55071b4e1e34b107bd58e0c35efa10eb84d3b7
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-23 08:57:12 +0100

    Product added to cart with proper quantity and price calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,8 +4,10 @@ const Cart = ({ cart }) => {
   // console.log(cart);
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
   const tax = (total * 0.1).toFixed(2);
@@ -13,7 +15,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected items: {cart.length} </p>
+      <p>Selected items: {quantity} </p>
       <p>Total Price: ${total}</p>
       <p>Total shipping: ${shipping} </p>
       <p>Tax: {tax}</p>
