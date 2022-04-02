e3d11e1c6756e736149be3bfc26d06f11f64d39a
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-04-02 14:12:27 +0200

    Order summary update with review buton

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,21 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useCart from "../../hooks/UseCart";
 import useProducts from "../../hooks/useProducts";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useCart(products);
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+    removeFromDb(product.id);
+  };
   return (
     <div className="shop-container">
-      <div className="review-item-container">
+      <div className="review-items-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem key={product.id} product={product}>
+            handleRemoveProduct={handleRemoveProduct}
+          </ReviewItem>
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart}>
+          <Link to="/inventory">
+            <button>Proceed CheckOut</button>
+          </Link>
+        </Cart>
       </div>
     </div>
   );
